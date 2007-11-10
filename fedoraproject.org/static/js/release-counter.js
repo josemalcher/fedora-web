7f559af272f5883af81c8b6c9e0f0de044cf5972
Ricky Zhou <ricky@fedoraproject.org>
2007-11-10 16:57:25 -0500

    Clean up directory structure, modify all files to point correctly, add Fedora GPG key.

---
+++ fedoraproject.org/static/js/release-counter.js
@@ -0,0 +1,12 @@
+var today = new Date();
+var release = new Date("November 8, 2007 14:00:00 UTC");
+var millisBetweenDates = release - today;
+var days = Math.ceil(millisBetweenDates/1000/60/60/24);
+if (days < 0)
+{
+  document.write('<div class="fedora-banner"><a href="http://fedoraproject.org/wiki/Releases/8/Schedule"><img src="http://fedoraproject.org/images/banners/f8-banner-animation.gif" alt="Fedora 8 is here!" style="border: none;"></a></div>');
+}
+else
+{
+  document.write('<div class="fedora-banner"><a href="http://fedoraproject.org/wiki/Releases/8/Schedule"><img src="http://fedoraproject.org/static/images/counter/' + days +'.png" alt="Fedora 8 Werewolf unleased in ' + days + ' days." style="border: none;"></a></div>');
+}
