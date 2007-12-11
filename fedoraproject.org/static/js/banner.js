c4ae5bf6b129328aa468fe4ebb1e532c4af99948
Ricky Zhou <ricky@fedoraproject.org>
2007-12-10 20:50:32 -0500

    Corrected banner rotation.

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -4,19 +4,16 @@ var banners = [
 ];
 
 window.onload = function() {
-  var total = 0;
-  for (var i = 0; i < banners.length; ++i) { total += banners[i][3]; }
-
   var choices = [];
   var k = 0;
   for (var i = 0; i < banners.length; ++i) {
-      for (var j = 0; j < banners[i][3]; ++j) { choices[++k] = i; }
+      for (var j = 0; j < banners[i][3]; ++j) { choices[k++] = i; }
   }
 
-  var choice = Math.floor(Math.random()*(choices.length+1))
-  var image = banners[choices[choice]][0]
-  var alt = banners[choices[choice]][1]
-  var url = banners[choices[choice]][2]
+  var choice = Math.floor(Math.random()*(choices.length));
+  var image = banners[choices[choice]][0];
+  var alt = banners[choices[choice]][1];
+  var url = banners[choices[choice]][2];
 
   var bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
   bannerlink.setAttribute("href", url);
