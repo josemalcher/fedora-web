92620433fcc002df6c28c260a2e4cb6c34f56d72
Ignacio Vazquez-Abrams <ivazqueznet+git@gmail.com>
2008-01-22 10:35:30 -0500

    Whoops. I'll get better at this, I promise.

--- fedoraproject.org/static/js/release-counter.js
+++ fedoraproject.org/static/js/release-counter.js
@@ -4,7 +4,7 @@ var millisBetweenDates = release - today;
 var days = Math.ceil(millisBetweenDates/1000/60/60/24);
 if (days < 0)
 {
-  document.write('<div class="fedora-banner"><a href="http://fedoraproject.org/wiki/Releases/8/Schedule"><img src="http://fedoraproject.org/images/banners/f8-banner-animation.gif" alt="Fedora 8 is here!" style="border: none;"></a></div>');
+  document.write('<div class="fedora-banner"><a href="http://fedoraproject.org/wiki/Releases/8/Schedule"><img src="http://fedoraproject.org/static/images/banners/f8-banner-animation.gif" alt="Fedora 8 is here!" style="border: none;"></a></div>');
 }
 else
 {
