6c87eb8f6c4d9f3f192a70c492ae4c00499a2da5
Ricky Zhou <ricky@fedoraproject.org>
2008-03-27 23:35:53 -0400

    Add caching for fetching feeds.

--- fedoraproject.org/build/rss.py
+++ fedoraproject.org/build/rss.py
@@ -1,7 +1,21 @@
+import os
+import cPickle
 import feedparser
 
 # TODO: This fetches once for every build.py run.  Need to make it
 # cache the results (pickle?) and delete after a full website build.  
+
 def feedparse(url):
-    return feedparser.parse(url)
+    feeds = dict()
+    cachefile = os.path.join(os.getcwd(), 'build/rss.cache')
+    if os.path.isfile(cachefile):
+        f = open(cachefile)
+        feeds = cPickle.load(f)
+        f.close()
+    if not feeds.has_key(url):
+        feeds[url] = feedparser.parse(url)
+        f = open(cachefile, 'w')
+        cPickle.dump(feeds, f)
+        f.close()
+    return feeds[url]
 
