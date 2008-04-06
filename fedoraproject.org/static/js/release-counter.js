d6341ad3090f05b102a40a3d9229dec77dcdfed6
Ricky Zhou <ricky@fedoraproject.org>
2008-04-06 16:05:35 -0400

    Make javascript run onload.

--- fedoraproject.org/static/js/release-counter.js
+++ fedoraproject.org/static/js/release-counter.js
@@ -1,23 +1,22 @@
-var today = new Date();
-var release = new Date("April 29, 2008 14:00:00 UTC");
-var millisBetweenDates = release - today;
-var days = Math.ceil(millisBetweenDates/1000/60/60/24);
+window.onload = function() {
+    var today = new Date();
+    var release = new Date("April 29, 2008 14:00:00 UTC");
+    var millisBetweenDates = release - today;
+    var days = Math.ceil(millisBetweenDates/1000/60/60/24);
 
-var url = "http://fedoraproject.org/wiki/Releases/9/Schedule"
-var bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
-bannerlink.setAttribute("href", url);
+    var url = "http://fedoraproject.org/wiki/Releases/9/Schedule"
+      var bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
+    bannerlink.setAttribute("href", url);
 
-var bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
+    var bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
 
-if (days < 0)
-{
-  // TODO: Get an actual image here!
-  bannerimg.setAttribute("src", "http://fedoraproject.org/static/images/banner/f9release.png");
-  bannerimg.setAttribute("alt", "Fedora 9 is here!");
-}
-else
-{
-  bannerimg.setAttribute("src", "http://fedoraproject.org/static/images/counter/" + (days < 10? '0' + days : days) + ".png");
-  bannerimg.setAttribute("alt", "Fedora 9 Sulfur released in " + days + "days.");
+    if (days < 0) {
+        // TODO: Get an actual image here!
+        bannerimg.setAttribute("src", "http://fedoraproject.org/static/images/banner/f9release.png");
+        bannerimg.setAttribute("alt", "Fedora 9 is here!");
+    } else {
+        bannerimg.setAttribute("src", "http://fedoraproject.org/static/images/counter/fedora9-countdown-" + (days < 10? '0' + days : days) + ".png");
+        bannerimg.setAttribute("alt", "Fedora 9 Sulfur released in " + days + " days.");
+    }
 }
 
