1683df503effaaf726f1db8a51b91530199b799b
Ricky Zhou <ricky@fedoraproject.org>
2008-09-30 17:05:15 -0400

    Add f10 beta banner.

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -1,5 +1,6 @@
 var banners = [
-  ["http://fedoraproject.org/static/images/banners/f9release.png", "Fedora 9 Sulphur is here!", "http://fedoraproject.org/get-fedora", 4]
+  ["http://fedoraproject.org/static/images/banners/f9release.png", "Fedora 9 Sulphur is here!", "http://fedoraproject.org/get-fedora", 1],
+  ["http://fedoraproject.org/static/images/banners/f10beta.png", "Fedora 10 Beta is available!", "http://fedoraproject.org/get-prerelease", 1]
 //  ["http://fedoraproject.org/static/images/banners/paulfrieldstv2.png", "Paul Frields talks Fedora 9", "http://www.redhatmagazine.com/2008/05/15/video-fedora-project-leader-on-fedora-9/", 3]
 //  ["http://fedoraproject.org/static/images/banners/fedorawn.png", "Fedora Weekly News", "http://fedoraproject.org/wiki/FWN/LatestIssue", 1],
 //  ["http://fedoraproject.org/static/images/banners/fedora.org_devfu_widget.jpg", "Dev Fu", "http://developer.redhatmagazine.com/", 1],
