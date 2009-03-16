249774d2463f71cec5ce0567dd34c0ad4912cc96
Ricky Zhou <ricky@fedoraproject.org>
2009-03-16 15:09:02 -0400

    Removing extra comma.

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -2,7 +2,7 @@ var banners = [
   ["http://fedoraproject.org/static/images/banners/f10release.png", "Fedora 10 Cambridge is here!", "http://fedoraproject.org/get-fedora", 4],
   ["http://fedoraproject.org/static/images/banners/fedora10_rhm_widget.png", "Fedora 10 Cambridge - Fire it up.", "http://www.redhatmagazine.com/2008/12/02/video-fedora-10/", 1],
   ["http://fedoraproject.org/static/images/banners/f11alpha.png", "Fedora 11 Alpha", "http://fedoraproject.org/get-prerelease", 4],
-  ["http://fedoraproject.org/static/images/banners/picturebanner-envelope-1.png", "Fedora Picture Book - Now accepting submissions", "https://fedoraproject.org/wiki/Category:Picture_book", 4],
+  ["http://fedoraproject.org/static/images/banners/picturebanner-envelope-1.png", "Fedora Picture Book - Now accepting submissions", "https://fedoraproject.org/wiki/Category:Picture_book", 4]
 //  ["http://fedoraproject.org/static/images/banners/paulfrieldstv2.png", "Paul Frields talks Fedora 9", "http://www.redhatmagazine.com/2008/05/15/video-fedora-project-leader-on-fedora-9/", 3]
 //  ["http://fedoraproject.org/static/images/banners/fedorawn.png", "Fedora Weekly News", "http://fedoraproject.org/wiki/FWN/LatestIssue", 1],
 //  ["http://fedoraproject.org/static/images/banners/fedora.org_devfu_widget.jpg", "Dev Fu", "http://developer.redhatmagazine.com/", 1],
