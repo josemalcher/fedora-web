8f3dd95e2d2720f617af5ee87dfb7a351a66ba85
Ricky Zhou <ricky@fedoraproject.org>
2009-05-25 14:05:11 -0400

    Automatically generate Apache configuration.

--- fedoraproject.org/build/translations.py
+++ fedoraproject.org/build/translations.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python
 import sys
 
-file = sys.argv[1]
-f = open(file)
+linguasfile = sys.argv[1]
+f = open(linguasfile)
 print '''
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml"
