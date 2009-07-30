8fd631050b83f4a922ddc45207514e0034359dd4
Ricky Zhou <ricky@fedoraproject.org>
2009-07-30 17:10:56 -0400

    Fix copyright message.

--- fedorapeople.org/make-people-page.sh
+++ fedorapeople.org/make-people-page.sh
@@ -87,7 +87,7 @@ cat << EOM>> $outfile
     <div id="bottom">
       <div id="footer">
         <p class="copy">
-        Copyright &copy; 2009 Red Hat, Inc. and others.  All Rights Reserved.
+        &copy; 2009 Red Hat, Inc. and others.  
         Please send any comments or corrections to the <a href="mailto:webmaster@fedoraproject.org">websites team</a>.
         </p>
         <p class="disclaimer">
