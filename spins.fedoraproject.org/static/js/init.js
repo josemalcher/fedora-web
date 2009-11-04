dd4f96f254c9d982e51cf06d06558b2c52491c32
Sijis Aviles <sijis@fedoraproject.org>
2009-11-03 23:41:36 -0600

    added hover functionality for "learn more and download" links on index page

--- spins.fedoraproject.org/static/js/init.js
+++ spins.fedoraproject.org/static/js/init.js
@@ -2,5 +2,19 @@
 // load tabs on pageload 
 $(function() { 
 	// setup ul.tabs to work as tabs for each div directly under div.panes 
-	$("ul.tabs").tabs("div.panes > div"); 
+	$("ul.tabs").tabs("div.panes > div");
+	
+	// hide all "learn more" links
+	$("span.learn-more").hide();
+	
+	// toggles links on/off on hover
+	$("tr.spin-row").hover(
+		function () {
+			$(this).find("span.learn-more").show();
+		}, 
+		function () {
+			$(this).find("span.learn-more").hide();
+		}
+	);
+
 });
