6d77ceee5eadb1563997b6edc54903a3824c817f
Todd Zullinger <tmz@pobox.com>
2009-11-25 15:54:01 -0500

    Add javascript for Open Source Lab sponsor logo
    (This should have gone with adding the image.)

---
+++ fedoraproject.org/static/js/sponsors/osuosl.js
@@ -0,0 +1,21 @@
+var sponsor = "osuosl";
+
+var sponsor_banner = [
+    "/static/images/sponsors/sidebar/" + sponsor + ".png",
+    "Open Source Lab",
+    "http://osuosl.org/"
+];
+
+window.onload = function() {
+
+  var image = sponsor_banner[0];
+  var alt = sponsor_banner[1];
+  var url = sponsor_banner[2];
+
+  var bannerlink = document.getElementById("hosting-sponsor").getElementsByTagName("a")[0];
+  bannerlink.setAttribute("href", url);
+
+  var bannerimg = document.getElementById("hosting-sponsor").getElementsByTagName("img")[0];
+  bannerimg.setAttribute("src", image);
+  bannerimg.setAttribute("alt", alt);
+}
