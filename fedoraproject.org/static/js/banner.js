dd588d50cbd07b76deaf705c2f7c30cfcf73b2dc
Sijis Aviles <sijis@fedoraproject.org>
2010-03-10 14:28:16 -0600

    fixing js display issues for sponsor banner section - consolidated all windows.onload to custom.js - remove window.onload calls from other files (banner.js, sponsors/*.js)

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -28,24 +28,3 @@ var banners = [
     0
   ]
 ];
-
-window.onload = function() {
-  var choices = [];
-  var k = 0;
-  for (var i = 0; i < banners.length; ++i) {
-      for (var j = 0; j < banners[i][3]; ++j) { choices[k++] = i; }
-  }
-
-  var choice = Math.floor(Math.random()*(choices.length));
-  var image = banners[choices[choice]][0];
-  var alt = banners[choices[choice]][1];
-  var url = banners[choices[choice]][2];
-
-  var bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
-  bannerlink.setAttribute("href", url);
-
-  var bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
-  bannerimg.setAttribute("src", image);
-  bannerimg.setAttribute("alt", alt);
-}
-
--- fedoraproject.org/static/js/custom.js
+++ fedoraproject.org/static/js/custom.js
@@ -1,5 +1,8 @@
-// for export regulations
+// this executes all the "window.onload" type events
+
 $(document).ready(function(){
+
+	// for export regulations
 	$("#export-regulations").expander({
 		slicePoint: 350,
 		widow: 1,
@@ -7,4 +10,41 @@ $(document).ready(function(){
 		expandText: '<strong>Read full export regulations</strong> <img src="/static/images/arrow_down.png">',
 		userCollapseText: '<strong>Hide full export regulations</strong> <img src="/static/images//arrow_up.png">'
 	});
+
+
+	// main site banners
+	// see: banner.js
+	var choices = [];
+	var k = 0;
+	for (var i = 0; i < banners.length; ++i) {
+		for (var j = 0; j < banners[i][3]; ++j) { choices[k++] = i; }
+	}
+
+	var choice = Math.floor(Math.random()*(choices.length));
+	var b_image = banners[choices[choice]][0];
+	var b_alt = banners[choices[choice]][1];
+	var b_url = banners[choices[choice]][2];
+
+	var b_bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
+	b_bannerlink.setAttribute("href", b_url);
+
+	var b_bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
+	b_bannerimg.setAttribute("src", b_image);
+	b_bannerimg.setAttribute("alt", b_alt);
+
+
+	// hosting sponsor banners
+	// see: /sponsors/*.js
+	var s_image = sponsor_banner[0];
+	var s_alt = sponsor_banner[1];
+	var s_url = sponsor_banner[2];
+
+	var s_bannerlink = document.getElementById("hosting-sponsor").getElementsByTagName("a")[0];
+	s_bannerlink.setAttribute("href", s_url);
+
+	var s_bannerimg = document.getElementById("hosting-sponsor").getElementsByTagName("img")[0];
+	s_bannerimg.setAttribute("src", s_image);
+	s_bannerimg.setAttribute("alt", s_alt);
+
+
 });
