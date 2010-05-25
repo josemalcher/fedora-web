ccc6029fba88453b0f399263baf41467c7f90443
Nick Bebout <nick@bebout.net>
2010-05-24 23:15:22 -0400

    Add strings to translate

---
+++ fedoraproject.org/data/totranslate.py
@@ -0,0 +1,7 @@
+${Markup(_('&lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/sn-making-media.html"&gt;How do I use this file?&lt;/a&gt;'))}
+${Markup(_('&lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/ch-upgrade-x86.html"&gt;Fedora 13 Upgrade Guide&lt;/a&gt;'))}
+${Markup(_('Already have Fedora and want to upgrade to Fedora 13? Check out the &lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/ch-upgrade-x86.html"&gt;Upgrading Fedora&lt;/a&gt; instructions, and download the DVD ISO you\'ll need to start the upgrade process below.'))}
+${Markup(_('&lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/sn-making-media.html"&gt;How do I use this file?&lt;/a&gt;'))}
+${Markup(_('Not sure which of these architectures are right for you? Need help figuring out what a particular computer\'s architecture is? We\'ve &lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/ch-new-users.html#sn-which-arch"&gt;got a helpful guide&lt;/a&gt;.'))}
+${Markup(_('For help determining what architecture your system is, &lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Installation_Guide/ch-new-users.html#sn-which-arch"&gt;please view our architecture help guide&lt;/a&gt;.'))}
+${Markup(_('Windows user?  Follow &lt;a href="http://docs.fedoraproject.org/en-US/Fedora/13/html/Burning_ISO_images_to_disc/sect-Burning_ISO_images_to_disc-Validating_the_Files.html"&gt;these instructions&lt;/a&gt; instead.'))}
