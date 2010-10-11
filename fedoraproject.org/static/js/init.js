f0e397879eab3a138c6e0ba09d95cfe95901749b
Ricky Zhou <ricky@fedoraproject.org>
2010-10-11 16:15:05 -0400

    Ugly hack to get sponsor banners working again.
    We should clean up the banner code to make it more modular/easy to
    enable and disable various banners separately.

--- fedoraproject.org/static/js/init.js
+++ fedoraproject.org/static/js/init.js
@@ -33,36 +33,40 @@ $(document).ready(function(){
 
 	// main site banners
 	// see: banner.js
-	var choices = [];
-	var k = 0;
-	for (var i = 0; i < banners.length; ++i) {
-		for (var j = 0; j < banners[i][3]; ++j) { choices[k++] = i; }
-	}
+    if (typeof banners != "undefined") {
+        var choices = [];
+        var k = 0;
+        for (var i = 0; i < banners.length; ++i) {
+            for (var j = 0; j < banners[i][3]; ++j) { choices[k++] = i; }
+        }
 
-	var choice = Math.floor(Math.random()*(choices.length));
-	var b_image = banners[choices[choice]][0];
-	var b_alt = banners[choices[choice]][1];
-	var b_url = banners[choices[choice]][2];
+        var choice = Math.floor(Math.random()*(choices.length));
+        var b_image = banners[choices[choice]][0];
+        var b_alt = banners[choices[choice]][1];
+        var b_url = banners[choices[choice]][2];
 
-	var b_bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
-	b_bannerlink.setAttribute("href", b_url);
+        var b_bannerlink = document.getElementById("banner").getElementsByTagName("a")[0];
+        b_bannerlink.setAttribute("href", b_url);
 
-	var b_bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
-	b_bannerimg.setAttribute("src", b_image);
-	b_bannerimg.setAttribute("alt", b_alt);
+        var b_bannerimg = document.getElementById("banner").getElementsByTagName("img")[0];
+        b_bannerimg.setAttribute("src", b_image);
+        b_bannerimg.setAttribute("alt", b_alt);
+    }
 
 
 	// hosting sponsor banners
 	// see: /sponsors/*.js
-	var s_image = sponsor_banner[0];
-	var s_alt = sponsor_banner[1];
-	var s_url = sponsor_banner[2];
+    if (typeof sponsor_banner != "undefined") {
+        var s_image = sponsor_banner[0];
+        var s_alt = sponsor_banner[1];
+        var s_url = sponsor_banner[2];
 
-	var s_bannerlink = document.getElementById("hosting-sponsor").getElementsByTagName("a")[0];
-	s_bannerlink.setAttribute("href", s_url);
+        var s_bannerlink = document.getElementById("hosting-sponsor").getElementsByTagName("a")[0];
+        s_bannerlink.setAttribute("href", s_url);
 
-	var s_bannerimg = document.getElementById("hosting-sponsor").getElementsByTagName("img")[0];
-	s_bannerimg.setAttribute("src", s_image);
-	s_bannerimg.setAttribute("alt", s_alt);
+        var s_bannerimg = document.getElementById("hosting-sponsor").getElementsByTagName("img")[0];
+        s_bannerimg.setAttribute("src", s_image);
+        s_bannerimg.setAttribute("alt", s_alt);
+    }
 
 });
