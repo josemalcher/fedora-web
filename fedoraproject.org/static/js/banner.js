adb61a5311fece43a767017c51ff69890fb50ada
Sijis Aviles <sijis@fedoraproject.org>
2010-11-01 22:11:47 -0500

    added f14 release banner

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -4,25 +4,19 @@ var banners = [
 //  Target URL
 //  Weight
   [
-    "https://fedoraproject.org/static/images/banners/f13release.png",
-    "Fedora 13 Goddard is here!",
+    "/static/images/banners/f14release.png",
+    "Fedora 14 Laughlin is here!",
     "https://fedoraproject.org/get-fedora",
     5
   ],
   [
-    "https://fedoraproject.org/static/images/banners/f14beta.png",
-    "Fedora 14 Beta",
-    "https://fedoraproject.org/get-prerelease",
-    5
-  ],
-  [
-    "https://fedoraproject.org/static/images/banners/picturebanner-envelope-1.png",
+    "/static/images/banners/picturebanner-envelope-1.png",
     "Fedora Picture Book - Now accepting submissions",
     "https://fedoraproject.org/wiki/Category:Picture_book",
     0
   ],
   [
-    "https://fedoraproject.org/static/images/banners/super-packager.png",
+    "/static/images/banners/super-packager.png",
     "Fedora Community",
     "https://admin.fedoraproject.org/community/",
     0
