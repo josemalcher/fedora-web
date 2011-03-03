db3c7d4b653e71fab3db379f5959f26c8066f41d
Sijis Aviles <sijis@fedoraproject.org>
2011-03-03 10:57:54 -0600

    added f15 alpha banner and get-prelease link to get-fedora

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -10,6 +10,12 @@ var banners = [
     5
   ],
   [
+    "/static/images/banners/f15alpha.png",
+    "Fedora 15 Alpha",
+    "https://fedoraproject.org/get-prerelease",
+    5
+  ],
+  [
     "/static/images/banners/picturebanner-envelope-1.png",
     "Fedora Picture Book - Now accepting submissions",
     "https://fedoraproject.org/wiki/Category:Picture_book",
