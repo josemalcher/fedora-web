8cc42c697355edf7531edb9347a770ceca6f4d4a
Sijis Aviles <sijis@fedoraproject.org>
2011-04-17 17:23:35 -0500

    updating release counter for f15

--- fedoraproject.org/static/js/release-counter-ext.js
+++ fedoraproject.org/static/js/release-counter-ext.js
@@ -1,8 +1,8 @@
-var available_langs = [ "ar", "bg", "bn_IN", "cs", "da", "de", "el", "en", "es", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "is", "it", "ja", "kn", "ko", "ks", "ml", "nl", "pa", "pl", "pt_BR", "pt", "ro", "ru", "si", "sr", "sv", "th", "tr", "uk", "zh_CN", "zh_TW" ];
+var available_langs = [ "ar", "bg", "bn_IN", "cs", "da", "de", "el", "en", "es", "fi", "fr", "gu", "he", "hi", "hr", "hu", "id", "is", "it", "ja", "kn", "ko", "ks", "ml", "nl", "pa", "pl", "pt", "pt_BR", "ro", "ru", "si", "sr", "sv", "th", "tr", "uk", "zh_CN", "zh_TW" ];
 
 
 var today = new Date();
-var release = new Date("Nov 2, 2010 15:00:00 UTC");
+var release = new Date("May 24, 2011 15:00:00 UTC");
 var millisBetweenDates = release - today;
 var days = Math.ceil(millisBetweenDates/1000/60/60/24);
 
@@ -19,7 +19,7 @@ if (lang_match) {
     }
 }
 
-var url = "https://fedoraproject.org/wiki/Releases/14/Schedule";
+var url = "https://fedoraproject.org/wiki/Releases/15/Schedule";
 
 var banner = document.createElement('div');
 
@@ -30,12 +30,12 @@ bannerimg.style.border = "none";
 
 if (days <= 0) {
     // TODO: Get an actual image here!
-    bannerimg.setAttribute("src", "https://fedoraproject.org/static/images/banners/f14release.png");
-    bannerimg.setAttribute("alt", "Fedora 14 is here!");
-    url = "https://fedoraproject.org/get-fedora";
+    bannerimg.setAttribute("src", "https://fedoraproject.org/static/images/banners/f15release.png");
+    bannerimg.setAttribute("alt", "Fedora 15 is here!");
+    url = "http://get.fedoraproject.org/";
 } else {
-    bannerimg.setAttribute("src", "https://fedoraproject.org/static/images/counter/" + lang + "/fedora14-countdown-banner-" + days + "." + lang + "." + "png");
-    bannerimg.setAttribute("alt", "Fedora 14 Laughlin released in " + days + " days.");
+    bannerimg.setAttribute("src", "https://fedoraproject.org/static/images/counter/" + lang + "/fedora15-countdown-banner-" + days + "." + lang + "." + "png");
+    bannerimg.setAttribute("alt", "Fedora 15 Lovelock released in " + days + " days.");
 }
 
 bannerlink.setAttribute("href", url);
