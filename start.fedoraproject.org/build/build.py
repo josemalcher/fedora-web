dd3f5a718ae0c1821f267331d351c32cd04918eb
Elad Alfassa <elad@fedoraproject.org>
2011-04-29 12:43:12 +0300

    parallel make for start.fpo as well

--- start.fedoraproject.org/build/build.py
+++ start.fedoraproject.org/build/build.py
@@ -7,16 +7,9 @@ Some code/design taken from python.org's website build script
 (https://svn.python.org/www/trunk/beta.python.org/build/new-build/)
 '''
 
-import os
-import sys
-import timing
-import re
-import shutil
-import csv
-import iniparse
-import urlparse
-import operator
-import locale
+import os, sys, timing, re, shutil
+
+from pkg_resources import get_distribution 
 
 from optparse import OptionParser
 
@@ -25,15 +18,8 @@ from gettext import GNUTranslations
 from genshi.filters import Translator
 from genshi.template import TemplateLoader
 
-from genshi.core import Markup
-
 from rss import *
-
-locale.setlocale(locale.LC_COLLATE, 'en_US')
-
-vcsprefix = {
-  'hg': '/'
-}
+import fileinput
 
 def process(args):
     if os.path.exists(options.output) and options.erase:
@@ -47,92 +33,53 @@ def process(args):
             if os.path.exists(outpath):
                 shutil.rmtree(outpath)
             copytree(dir, outpath)
-    if options.trac and os.path.isdir(options.trac):
-        projects = read_trac(options.trac)
-    else:
-        if options.data:
-            projects = read_data(options.data)
-        else:
-            projects = []
-    projects.sort(key=operator.itemgetter('desc'), cmp=locale.strcoll)
-    timing.start()
-    for dirpath, dirnames, filenames in os.walk(options.input):
-        try:
-            process_dir(dirpath, filenames, projects)
-        except:
-            if options.keepgoing:
-                print 'Error!'
-            else:
-                raise
-    timing.finish()
-    print 'Website build time: %s' % timing.milli()
+    if options.input is not None:
+        timing.start()
+        for dirpath, dirnames, filenames in os.walk(options.input):
+            try:
+                process_dir(dirpath, filenames)
+            except:
+                if options.keepgoing:
+                    print 'Error!'
+                else:
+                    raise
+        timing.finish()
+        if not options.rss:
+            print 'Website build time: %s' % timing.milli()
 
-def read_data(filename):
-    projects = []
-    for line in csv.reader(open(filename, 'r')):
-        if not line[0].startswith('#'):
-            project = {
-                'id': line[0],
-                'url': 'https://fedorahosted.org/%s/' % (line[0]),
-                'desc': line[1].decode('utf-8'),
-                'title': line[1].decode('utf-8')
-            }
-            if len(line) > 2:
-                project['vcs'] = line[2]
-                project['vcsprefix'] = vcsprefix.get(project['vcs'], '')
-                project['vcsweburl'] = 'https://fedorahosted.org/%s/browser/' % (line[0])
-            projects.append(project)
-    return projects
-
-def read_trac(path):
-    projects = []
-    for dir in os.listdir(path):
-        filename = os.path.join(path, dir, 'conf', 'trac.ini')
-        if os.path.isfile(filename):
-            conf = iniparse.INIConfig(file(filename, 'r'))
-            project = {
-                'id': conf['trac']['base_url'].rstrip('/').split('/')[-1],
-                'url': conf['trac']['base_url'],
-                'desc': conf['project']['name'].decode('utf-8'),
-                'title': conf['project']['descr'].decode('utf-8')
-            }
-            if 'repository_type' in conf['trac']:
-                project['vcs'] = conf['trac']['repository_type']
-                project['vcsprefix'] = vcsprefix.get(project['vcs'], '')
-                project['vcsbase'] = os.path.basename(
-                    conf['trac']['repository_dir'])
-                project['vcsweburl'] = urlparse.urljoin(
-                    conf['trac']['base_url'].rstrip('/') + '/', 'browser')
-            projects.append(project)
-    return projects
-
-def process_dir(dirpath, filenames, projects):
+def process_dir(dirpath, filenames):
     '''
     Process a directory
     '''
-    translations = GNUTranslations(open(os.path.join(options.podir, options.lang + '.mo')))
-    loader = TemplateLoader(['.'], callback=lambda template: template.filters.insert(0, Translator(translations.ugettext)))
+    if options.podir and options.lang:
+        translations = GNUTranslations(open(os.path.join(options.podir, options.lang + '.mo')))
+        if int(get_distribution('genshi').version[2]) < 6:
+            loader = TemplateLoader(['.'], callback=lambda template: template.filters.insert(0, Translator(translations.ugettext)))
+        else:
+            loader = TemplateLoader(['.'], callback=lambda template: template.filters.insert(0, Translator(translations)))
     for fn in filenames:
         if fn.endswith('~') or fn.endswith('.swp'):
             continue
         src_file = os.path.join(dirpath, fn)
+        if options.rss:
+            for line in fileinput.input(src_file):
+                if line.find('feedparse')>0:
+                    match = re.split('^.*feedparse\(\'', line)
+                    feedurl = re.split('\'\)', match[1])
+                    feedparse(feedurl[0])
+            continue;
         dest_file = os.path.join(options.output, src_file[len(options.input):]) + '.' + options.lang # Hideous
         curpage = src_file[len(options.input):].rstrip('.html')
-        relpath = '../' * (dest_file.count('/') - 1)
-        relpath = relpath.rstrip('/')
-        if relpath == '': relpath = '.'
         if not os.path.exists(os.path.dirname(dest_file)):
             os.makedirs(os.path.dirname(dest_file))
         template = loader.load(src_file)
         # Variables made availble to all templates
         page = template.generate(
-            _=lambda text: Markup(translations.ugettext(text)),
+            _=lambda text: translations.ugettext(text),
             feedparse=feedparse,
             lang=options.lang,
             path=options.basepath,
-            relpath=relpath,
             curpage=curpage,
-            projects=projects
             ).render(method='html', doctype='html')
         output = open(dest_file, 'w')
         output.write(page)
@@ -175,15 +122,16 @@ def main():
     parser.add_option('-e', '--erase',
         action='store_true', dest='erase', default=False,
         help='Erase any existing output directory first')
-    parser.add_option('-d', '--data', dest='data', default=None,
-        help='Filename to read project data from')
-    parser.add_option('-t', '--trac', dest='trac', default=None,
-        help='Path to read Trac project data from under')
+    parser.add_option('-r', '--rss-cache',
+        action='store_true', dest='rss', default=False,
+        help='Cache RSS feeds')
     base_path = os.path.dirname(os.path.abspath(__file__))
     (options, args) = parser.parse_args()
     options.basepath = options.basepath.rstrip('/')
-    options.input = options.input.rstrip('/') + '/'
-    options.output = options.output.rstrip('/') + '/'
+    if options.input is not None:
+        options.input = options.input.rstrip('/') + '/'
+    if options.output is not None:
+        options.output = options.output.rstrip('/') + '/'
     process(args)
 
 if __name__ == "__main__":
