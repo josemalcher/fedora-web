bdcaad653eba923c1b036e0d087824f64599027e
Sijis Aviles <sijis@fedoraproject.org>
2011-08-22 22:13:59 -0500

    Added f16 alpha banner

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -4,6 +4,12 @@ var banners = [
 //  Target URL
 //  Weight
   [
+    "/static/images/banners/f16alpha.png",
+    "Fedora 16 Verne Alpha is here!",
+    "https://fedoraproject.org/get-prerelease",
+    5
+  ],
+  [
     "/static/images/banners/f15release.png",
     "Fedora 15 Lovelock is here!",
     "https://fedoraproject.org/get-fedora",
