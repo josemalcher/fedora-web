f03bb5a2320abbc6b412811eacd50f83b2b707dc
shaiton <shaiton@fedoraproject.org>
2011-10-06 00:37:44 +0200

    F16 beta banner added and release counter updated

--- fedoraproject.org/static/js/release-counter-ext.js
+++ fedoraproject.org/static/js/release-counter-ext.js
@@ -2,7 +2,7 @@ var available_langs = [ "ar", "bg", "bn_IN", "cs", "da", "de", "el", "en", "es",
 
 
 var today = new Date();
-var release = new Date("October 25, 2011 15:00:00 UTC");
+var release = new Date("November 8, 2011 15:00:00 UTC");
 var millisBetweenDates = release - today;
 var days = Math.ceil(millisBetweenDates/1000/60/60/24);
 
