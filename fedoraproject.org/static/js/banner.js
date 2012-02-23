9b3f0e29e8f274e21e911deb444a6e760f84f55d
Kévin Raymond <shaiton@fedoraproject.org>
2012-02-23 22:43:10 +0100

    content updated for alpha. Still need to check get-prerelease URLs

--- fedoraproject.org/static/js/banner.js
+++ fedoraproject.org/static/js/banner.js
@@ -4,6 +4,12 @@ var banners = [
 //  Target URL
 //  Weight
   [
+    "/static/images/banners/f17alpha.png",
+    "Fedora 17 Beefy Miracle Alpha is here!",
+    "https://fedoraproject.org/get-prerelease",
+    5
+  ],
+  [
     "/static/images/banners/f16release.png",
     "Fedora 16 Verne is here!",
     "https://fedoraproject.org/get-fedora",
