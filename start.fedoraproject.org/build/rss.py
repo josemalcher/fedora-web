94e9b5bc2399ba347eab1213be44d7a8b25b8638
Kévin Raymond <shaiton@fedoraproject.org>
2012-03-05 20:03:49 +0100

    Fixing css-caching issues

--- start.fedoraproject.org/build/rss.py
+++ start.fedoraproject.org/build/rss.py
@@ -3,7 +3,7 @@ import cPickle
 import feedparser
 
 def feedparse(url):
-    feeds = dict()
+    feeds = [{}] 
     # TODO: Should the directory be hardcoded like this? 
     # Maybe use ConfigParser to get some stuff?
     cachefile = os.path.join(os.getcwd(), 'build/rss.cache')
@@ -12,9 +12,36 @@ def feedparse(url):
         feeds = cPickle.load(f)
         f.close()
     if not url in feeds:
-        feeds[url] = feedparser.parse(url)
+        main_feed = dict()
+        main_feed = feedparser.parse(url)
+        entry = [{}]
+        for feed in main_feed["entries"]:
+            '''We only need to save the link, title and date of blog posts'''
+            if "updated" in feed:
+                entry.append({'link':feed["link"], 'title':feed["title"], 'date':feed["date"]})
+            else:
+                entry.append({'link':feed["link"], 'title':feed["title"], 'date':""})
+        url_entries = [{}]
+        url_entries.append({'url':url, 'feed':entry})
+        feeds.append(url_entries)
+ 
         f = open(cachefile, 'w')
         cPickle.dump(feeds, f)
         f.close()
-    return feeds[url]
+    feed_url = dict()
+    err = True
+    # We parse the list
+    for i in feeds:
+        feed_url = i
+        # and here the dictionary
+        for j in feed_url:
+           try:
+               if j["url"] == url:
+                   err = False
+                   return j["feed"]   
+           except KeyError:
+               pass
 
+    if err is True:
+        print "ERROR we should have get a result"
+    return feeds
