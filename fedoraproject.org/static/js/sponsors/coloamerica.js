daa6c9287fcd2bebd8bc570cf1adf53013a8adba
Kevin Fenzi <kevin@scrye.com>
2012-07-24 09:38:58 -0600

    Update link for coloamerica to just the top website.

--- fedoraproject.org/static/js/sponsors/coloamerica.js
+++ fedoraproject.org/static/js/sponsors/coloamerica.js
@@ -3,5 +3,5 @@ var sponsor = "coloamerica";
 var sponsor_banner = [
     "/static/images/sponsors/sidebar/" + sponsor + ".png",
     "ColocationAmerica",
-    "http://www.colocationamerica.com/dedicated-servers/linux-dedicated.htm"
+    "http://www.colocationamerica.com/"
 ];
