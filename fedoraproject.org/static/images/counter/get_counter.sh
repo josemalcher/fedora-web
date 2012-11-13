f415cb462f93941e365d0c864411fdf566062d9d
Kévin Raymond <shaiton@fedoraproject.org>
2012-11-13 21:00:12 +0100

    adding the fedora18-countdown-banner files

--- fedoraproject.org/static/images/counter/get_counter.sh
+++ fedoraproject.org/static/images/counter/get_counter.sh
@@ -1,5 +1,24 @@
 #!/bin/sh
 
+set -e
+
+TARBALL=countdown-banners.tar.gz
+
+wget http://ryanlerch.fedorapeople.org/countdown-banner/$TARBALL
+tar xzf $TARBALL
+rm $TARBALL
+
+for i in `ls *.gz`
+do
+  lang=`echo $i |sed -n 's/fedora18-countdown-banner-\(.*\)\.tar.gz/\1/p'`
+  mkdir $lang
+  tar xzf $i -C $lang
+  rm $i
+done
+
+
+exit 0
+
 while read -r line
 do
    file="fedora17-countdown-banner-$line.tar.gz"
