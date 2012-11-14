61580b98c3ac6c171f9da1041c5eff9e2285ce26
Kévin Raymond <shaiton@fedoraproject.org>
2012-11-14 10:29:27 +0100

    correcting the fedora release number in JS

--- fedoraproject.org/static/js/release-counter-ext.js
+++ fedoraproject.org/static/js/release-counter-ext.js
@@ -5,7 +5,7 @@ var today = new Date();
 var release = new Date("January 08, 2013 15:00:00 UTC");
 var millisBetweenDates = release - today;
 var days = Math.ceil(millisBetweenDates/1000/60/60/24);
-var url = "https://fedoraproject.org/wiki/Releases/17/Schedule";
+var url = "https://fedoraproject.org/wiki/Releases/18/Schedule";
 
 var script = document.getElementById('fedora-banner');
 var lang = "en"
