0112bdd8db996a62f371667f6b7a921078a387a8
Robert Mayr <robyduck@fedoraproject.org>
2013-03-17 00:42:06 +0100

    add export regulations to spins.fp.o

--- spins.fedoraproject.org/static/js/init.js
+++ spins.fedoraproject.org/static/js/init.js
@@ -1,18 +1,25 @@
 
-// load tabs on pageload 
-$(function() { 
+// load tabs on pageload
+$(function() {
+	// for export regulations
+	$(".export-regulations").expander({
+		slicePoint: 350,
+		widow: 1,
+		userCollapse: true,
+		expandText: 'Read full export regulations',
+		userCollapseText: 'Hide full export regulations'});
 	// setup ul.tabs to work as tabs for each div directly under div.panes
 	// now remembers history
 	$("ul.tabs").tabs("div.panes > div").history();
-	
+
 	// hide all "learn more" links
 	$("span.learn-more").hide();
-	
+
 	// toggles links on/off on hover
 	$("tr.spin-row").hover(
 		function () {
 			$(this).find("span.learn-more").show();
-		}, 
+		},
 		function () {
 			$(this).find("span.learn-more").hide();
 		}
