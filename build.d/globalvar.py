c4586123300d2251356ef0c2daffddb5bb0b0858
Robert Mayr <robyduck@fedoraproject.org>
2013-12-13 21:34:11 +0100

    update ISO and image sizes in globalvar.py

--- build.d/globalvar.py
+++ build.d/globalvar.py
@@ -41,42 +41,42 @@ path={
 }
 
 iso_size={
-    'x86_64_DVD':          '4.2',       # In GB
-    'i386_DVD':            '4.3',       # In GB
-    'source_DVD':          '7.5',       # In GB
-    'i686_Live_Desktop':   '919',       # In MB
-    'x86_64_Live_Desktop': '951',       # In MB, default download from F18
-    'i686_Live_KDE':       '843',       # In MB
-    'x86_64_Live_KDE':     '878',       # In MB
-    'i686_Live_LXDE':      '656',       # In MB
-    'x86_64_Live_LXDE':    '691',       # In MB
-    'i686_Live_Xfce':      '588',       # In MB
-    'x86_64_Live_Xfce':    '621',       # In MB
-    'i386_Netinstall':     '353',       # In MB
-    'x86_64_Netinstall':   '317',       # In MB
-    'i686_Live_Security':  '730',       # In MB
-    'x86_64_Live_Security':'764',       # In MB
-    'i686_Live_Games':     '3.9',       # In GB
-    'x86_64_Live_Games':   '3.9',       # In GB
-    'i686_Live_Elab':      '2.0',       # In GB
-    'x86_64_Live_Elab':    '2.0',       # In GB
-    'i686_Live_Soas':      '599',       # In MB
-    'x86_64_Live_Soas':    '634',       # In MB
+    'x86_64_DVD':          '4.3',       # In GB
+    'i386_DVD':            '4.4',       # In GB
+    'source_DVD':          '9.2',       # In GB
+    'i686_Live_Desktop':   '922',       # In MB
+    'x86_64_Live_Desktop': '953',       # In MB, default download from F18
+    'i686_Live_KDE':       '896',       # In MB
+    'x86_64_Live_KDE':     '928',       # In MB
+    'i686_Live_LXDE':      '598',       # In MB
+    'x86_64_Live_LXDE':    '633',       # In MB
+    'i686_Live_Xfce':      '614',       # In MB
+    'x86_64_Live_Xfce':    '648',       # In MB
+    'i386_Netinstall':     '357',       # In MB
+    'x86_64_Netinstall':   '321',       # In MB
+    'i686_Live_Security':  '769',       # In MB
+    'x86_64_Live_Security':'805',       # In MB
+    'i686_Live_Games':     '3.7',       # In GB
+    'x86_64_Live_Games':   '3.7',       # In GB
+    'i686_Live_Elab':      '2.1',       # In GB
+    'x86_64_Live_Elab':    '2.1',       # In GB
+    'i686_Live_Soas':      '633',       # In MB
+    'x86_64_Live_Soas':    '668',       # In MB
     'i686_Live_Design':    '1.3',       # In GB
     'x86_64_Live_Design':  '1.3',       # In GB
-    'i686_Live_Sci-kde':   '2.7',       # In GB
-    'x86_64_Live_Sci-kde': '2.7',       # In GB
-    'i686_Live_Robotics':  '1.7',       # In GB
-    'x86_64_Live_Robotics':'1.7',       # In GB
+    'i686_Live_Sci-kde':   '3.4',       # In GB
+    'x86_64_Live_Sci-kde': '3.4',       # In GB
+    'i686_Live_Robotics':  '1.9',       # In GB
+    'x86_64_Live_Robotics':'1.9',       # In GB
     'i686_Live_Jam':       '1.5',       # In GB
     'x86_64_Live_Jam':     '1.5',       # In GB
-    'i686_Live_Mate':      '660',       # In MB
-    'x86_64_Live_Mate':    '694',       # In MB
+    'i686_Live_Mate':      '715',       # In MB
+    'x86_64_Live_Mate':    '750',       # In MB
     'PPC64_DVD':           '4.3',       # In GB
     'PPC64_Netinstall':    '333',       # In MB
     's390_DVD':            '4.0',       # In GB
-    'i686_sda.qcow2':      '225',       # In MB
-    'x86_64_sda.qcow2':    '226',       # In MB
-    'i686_raw':            '137',       # In MB
-    'x86_64_raw':          '129'        # In MB
+    'i686_sda.qcow2':      '201',       # In MB
+    'x86_64_sda.qcow2':    '204',       # In MB
+    'i686_raw':            '114',       # In MB
+    'x86_64_raw':          '116'        # In MB
 }
