a6c1702a529bd528e65235342c8dc54ca9a71f4a
Chaoyi Zha <summermontreal@gmail.com>
2014-03-10 17:48:38 -0400

    globalvar edit for new sizes
    Signed-off-by: Robert Mayr <robyduck@fedoraproject.org>

--- build.d/globalvar.py
+++ build.d/globalvar.py
@@ -45,14 +45,14 @@ iso_size={
     'x86_64_DVD':          '4.3',       # In GB
     'i386_DVD':            '4.4',       # In GB
     'source_DVD':          '9.2',       # In GB
-    'i686_Live_Desktop':   '922',       # In MB
-    'x86_64_Live_Desktop': '953',       # In MB, default download from F18
-    'i686_Live_KDE':       '896',       # In MB
-    'x86_64_Live_KDE':     '928',       # In MB
-    'i686_Live_LXDE':      '598',       # In MB
-    'x86_64_Live_LXDE':    '633',       # In MB
-    'i686_Live_Xfce':      '614',       # In MB
-    'x86_64_Live_Xfce':    '648',       # In MB
+    'i686_Live_Desktop':   '1000',       # In MB
+    'x86_64_Live_Desktop': '1000',       # In MB, default download from F18
+    'i686_Live_KDE':       '953',       # In MB
+    'x86_64_Live_KDE':     '1000',       # In MB
+    'i686_Live_LXDE':      '660',       # In MB
+    'x86_64_Live_LXDE':    '698',       # In MB
+    'i686_Live_Xfce':      '675',       # In MB
+    'x86_64_Live_Xfce':    '713',       # In MB
     'i386_Netinstall':     '357',       # In MB
     'x86_64_Netinstall':   '321',       # In MB
     'i686_Live_Security':  '769',       # In MB
@@ -61,8 +61,8 @@ iso_size={
     'x86_64_Live_Games':   '3.7',       # In GB
     'i686_Live_Elab':      '2.1',       # In GB
     'x86_64_Live_Elab':    '2.1',       # In GB
-    'i686_Live_Soas':      '633',       # In MB
-    'x86_64_Live_Soas':    '668',       # In MB
+    'i686_Live_Soas':      '695',       # In MB
+    'x86_64_Live_Soas':    '734',       # In MB
     'i686_Live_Design':    '1.3',       # In GB
     'x86_64_Live_Design':  '1.3',       # In GB
     'i686_Live_Sci-kde':   '3.4',       # In GB
@@ -71,8 +71,8 @@ iso_size={
     'x86_64_Live_Robotics':'1.9',       # In GB
     'i686_Live_Jam':       '1.5',       # In GB
     'x86_64_Live_Jam':     '1.5',       # In GB
-    'i686_Live_Mate':      '715',       # In MB
-    'x86_64_Live_Mate':    '750',       # In MB
+    'i686_Live_Mate':      '776',       # In MB
+    'x86_64_Live_Mate':    '814',       # In MB
     'PPC64_DVD':           '4.3',       # In GB
     'PPC64_Netinstall':    '340',       # In MB
     's390_DVD':            '4.6',       # In GB
